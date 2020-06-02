61b88c675dda0def17fa51b08e9f3622293c1e3e
Ke-vin Chan <chan9367@gmail.com>
2020-06-02 01:43:30 -0500

    added clickColor function and now you can click on a cell and it will turn into your selected color

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function addRow(){
     for(var i=0;i<colCount;i++){ //adds an entire row of cells based on how many column currently in grid
         var cell = document.createElement("td");
         row.appendChild(cell);
+        clickColor(cell);
     }
     
 }
@@ -23,8 +24,9 @@ function addCol(){
         var cell = document.createElement("td"); 
         cell.classList.add("cell");
         col[i].appendChild(cell);
+        //col[i].childElementCount is the same as how many columns there are
         colCount=col[i].childElementCount; //keep count of the columns, so when we add rows it will keep making cells in that row until its correspondant
-
+        clickColor(cell);
     }
 }
 function removeCol(){//remove the last added column when button is pressed
@@ -34,8 +36,15 @@ function removeCol(){//remove the last added column when button is pressed
     for(var i=0;i<col.length;i++){ //keep adding cells to the row of columns/cells
         if(col[i].childElementCount>2||grid.childElementCount>1){ //can't remove the row/column you start with
             col[i].removeChild(col[i].lastElementChild); //continuously removing last column of the last row
+            //col[i].childElementCount is the same as how many columns there are
             colCount=col[i].childElementCount; //keep count of the columns, so when we add rows it will keep making cells in that row until its correspondant
-
         }
     }               
+}
+var color = ""; 
+function clickColor(td){
+    function changeColor(){
+        td.style.backgroundColor = color;
+    }
+    td.addEventListener("click", changeColor);
 }
