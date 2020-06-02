3f2a3f7bb9a2f5b6d1246caa7172a7f84798b2bf
Ke-vin Chan <chan9367@gmail.com>
2020-06-01 22:20:48 -0500

    committed addRow functionality and set a base index.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function addRow(){
-    
+    var grid = document.getElementById("grid");
+    var row = document.createElement("tr");
+    row.classList.add("row");
+    grid.appendChild(row);
+    var cell = document.createElement("td");
+    row.appendChild(cell);
 }
