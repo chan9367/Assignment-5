1e1043c670af2fbbe4a7d19aaa7b99f09ccef0a4
Bayardo Zelaya <bayardozelaya@gmail.com>
2020-06-01 21:13:55 -0400

    added row and column function

---
+++ app.js
@@ -0,0 +1,14 @@
+//retrieve class column
+const container = document.getElementsByClassName(column);
+
+function makeRows(rows, cols) {
+  container.style.setProperty(`--grid-rows`, rows);
+  container.style.setProperty(`--grid-cols`, cols);
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.innerText = i + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(16, 16);
